c23fafa2117f828c61d0cfb1a9f9e35e4ced7392
Genival Lima <genivallimati@gmail.com>
2023-04-24 18:24:45 -0300

    alterando os arrays

---
+++ Arrays/atualizando-lista.js
@@ -0,0 +1,5 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Léo"];
+
+nomes.splice(1,2,"Rodrigo");
+
+console.log(nomes);
---
+++ Arrays/lista-duas-dimensoes.js
@@ -0,0 +1,8 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEmedias = [alunos, medias];
+
+console.log(`A aluna da posição 1 da lista de alunos é ${
+    listaDeAlunosEmedias[0][1]}. 
+    A nota da aluna é ${listaDeAlunosEmedias[1][1]}`);
