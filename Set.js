932cdb4e12dd24894bc1ad906adeb0c7b426c40e
Genivalti <genivallimati@gmail.com>
2023-05-10 16:44:30 -0300

    alterando novos arrays

---
+++ Set.js
@@ -0,0 +1,11 @@
+const nomes = ['Ana', 'Clara', 'Maria', 'Maria', 'João', 'João','João'];
+
+// const meuSet = new Set(nomes);
+
+// const nomesAtualizados = [...meuSet]
+
+// forma mais concisa 
+const nomesAtualizados = [...new Set(nomes)]
+
+
+console.log(nomesAtualizados);
